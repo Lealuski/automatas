473beb35a6bbdc0ac2d08a4153d0b192de2a732a
LealuskiMT <77172302+Lealuski@users.noreply.github.com>
2022-08-27 22:57:01 -0500

    ej2

--- Prueba.java
+++ Prueba.java
@@ -1,4 +1,3 @@
-
 package automatas;
 
 /**
@@ -6,9 +5,14 @@ package automatas;
  * @author David Leal
  */
 public class Prueba {
+
     public static void main(String[] args) {
-        char[] alf = {'a','b','c'};
-        String[] est = {"q0","S","T","R"};
+        ej2();
+    }
+
+    static void ej1() {
+        char[] alf = {'a', 'b', 'c'};
+        String[] est = {"q0", "S", "T", "R"};
         String[] estFinales = {"T"};
         String estIni = "q0";
         String[][] trans = new String[est.length][alf.length];
@@ -32,11 +36,39 @@ public class Prueba {
         String s5 = "bca";
         String s6 = "baba";
         afd.imprimirTabla();
-        System.out.println(s1+" es:"+afd.esAceptado(s1));
-        System.out.println(s2+" es:"+afd.esAceptado(s2));
-        System.out.println(s3+" es:"+afd.esAceptado(s3));
-        System.out.println(s4+" es:"+afd.esAceptado(s4));
-        System.out.println(s5+" es:"+afd.esAceptado(s5));
-        System.out.println(s6+" es:"+afd.esAceptado(s6));
+        System.out.println(s1 + " es:" + afd.esAceptado(s1));
+        System.out.println(s2 + " es:" + afd.esAceptado(s2));
+        System.out.println(s3 + " es:" + afd.esAceptado(s3));
+        System.out.println(s4 + " es:" + afd.esAceptado(s4));
+        System.out.println(s5 + " es:" + afd.esAceptado(s5));
+        System.out.println(s6 + " es:" + afd.esAceptado(s6));
+    }
+
+    static void ej2() {
+        char[] alf = {'0', '1'};
+        String[] est = {"q0", "q1", "q2"};
+        String[] estFinales = {"q2"};
+        String estIni = "q0";
+        String[][] trans = new String[est.length][alf.length];
+        trans[0][0] = "q1";
+        trans[0][1] = "q2";
+        trans[1][0] = "q0";
+        trans[1][1] = "q1";
+        trans[2][0] = "q1";
+        trans[2][1] = "q0";
+        AFD afd = new AFD(alf, est, estFinales, estIni, trans);
+        String s1 = "1";
+        String s2 = "111";
+        String s3 = "1001";
+        String s4 = "01";
+        String s5 = "0111";
+        String s6 = "101";
+        afd.imprimirTabla();
+        System.out.println(s1 + " es:" + afd.esAceptado(s1));
+        System.out.println(s2 + " es:" + afd.esAceptado(s2));
+        System.out.println(s3 + " es:" + afd.esAceptado(s3));
+        System.out.println(s4 + " es:" + afd.esAceptado(s4));
+        System.out.println(s5 + " es:" + afd.esAceptado(s5));
+        System.out.println(s6 + " es:" + afd.esAceptado(s6));
     }
 }
